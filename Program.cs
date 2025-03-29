d9df028fd36ddcaf83216be7a35eeb86e6df1b68
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-28 21:35:35 -0300

    Resolvido

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿double dinheiro, taxad, meses, total;
+
+Console.Clear();
+
+Console.WriteLine("Capital [c] (R$):");
+dinheiro = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Taxa de juros [i] (%):");
+taxad = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Tempo [t] (meses):");
+meses = Convert.ToDouble(Console.ReadLine());
+
+taxad = dinheiro * (taxad / 100) * meses;
+
+total = dinheiro + taxad;
+
+Console.WriteLine("Carregando...");
+Thread.Sleep(3000);
+
+Console.WriteLine($"Montante (R$): {total}");
